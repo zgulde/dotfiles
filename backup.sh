cbcde7e68d61025dfd09500048966226a6f6b283
zgulde <zach@Zachs-MacBook-Pro.local>
2015-11-19 14:41:53 -0600

    improved my backup script

--- backup.sh
+++ backup.sh
@@ -1,4 +1,23 @@
-cp ~/Library/Application\ Support/Karabiner/private.xml .
-cp ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Preferences.sublime-settings .
-cp ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Default\ \(OSX\).sublime-keymap .
-cp -a ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/snippets/. snippets/
+#!/bin/sh
+
+echo "last back up" > backup.log
+echo $(/bin/date) >> backup.log
+echo "Backing up..." >> backup.log
+echo "\n----------------------------------------------------" >> backup.log
+echo "\ncopying private.xml...\n" >> backup.log
+cp -v ~/Library/Application\ Support/Karabiner/private.xml . >> backup.log
+echo "\n----------------------------------------------------" >> backup.log
+echo "\ncopying sublime preferences...\n" >> backup.log
+cp -v ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Preferences.sublime-settings . >> backup.log
+echo "\n----------------------------------------------------" >> backup.log
+echo "\ncopying sublime keyboard shortcuts...\n" >> backup.log
+cp -v ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Default\ \(OSX\).sublime-keymap . >> backup.log
+echo "\n----------------------------------------------------" >> backup.log
+echo "\ncopying sublime snippets...\n" >> backup.log
+cp -v -a ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/snippets/. snippets/ >> backup.log
+echo "\n----------------------------------------------------" >> backup.log
+echo "backing up .bash_profile" >> backup.log
+cp -v ~/.bash_profile . >> backup.log
+
+
+less backup.log
