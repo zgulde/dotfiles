c1ae614cf48b9e425a3be6d30c2c54acee61c625
zgulde <zach@Zachs-MacBook-Pro.local>
2015-11-18 23:26:41 -0600

    updated settings

---
+++ backup.sh
@@ -0,0 +1,4 @@
+cp ~/Library/Application\ Support/Karabiner/private.xml .
+cp ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Preferences.sublime-settings .
+cp ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Default\ \(OSX\).sublime-keymap .
+cp -a ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/snippets/. snippets/
