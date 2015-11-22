0b4211d8782a80cb87603d54e1a50ec583eba471
zgulde <zach@Zachs-MacBook-Pro.local>
2015-11-21 23:28:36 -0600

    cowsay makes all shell scripts better

--- backup.sh
+++ backup.sh
@@ -2,23 +2,24 @@
 
 echo "last back up" > backup.log
 echo $(/bin/date) >> backup.log
-echo "Backing up..." >> backup.log
-echo "\n----------------------------------------------------" >> backup.log
-echo "\ncopying private.xml...\n" >> backup.log
+cowsay "Backing up..." >> backup.log
+echo "\n----------------------------------------------------\n" >> backup.log
+cowsay "copying private.xml..." >> backup.log
 cp -v ~/Library/Application\ Support/Karabiner/private.xml . >> backup.log
-echo "\n----------------------------------------------------" >> backup.log
-echo "\ncopying sublime preferences...\n" >> backup.log
+echo "\n----------------------------------------------------\n" >> backup.log
+cowsay "copying sublime preferences..." >> backup.log
 cp -v ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Preferences.sublime-settings . >> backup.log
-echo "\n----------------------------------------------------" >> backup.log
-echo "\ncopying sublime keyboard shortcuts...\n" >> backup.log
+echo "\n----------------------------------------------------\n" >> backup.log
+cowsay "copying sublime keyboard shortcuts..." >> backup.log
 cp -v ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Default\ \(OSX\).sublime-keymap . >> backup.log
-echo "\n----------------------------------------------------" >> backup.log
-echo "\ncopying sublime snippets...\n" >> backup.log
+echo "\n----------------------------------------------------\n" >> backup.log
+cowsay "copying sublime snippets..." >> backup.log
 cp -v -a ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/snippets/. snippets/ >> backup.log
-echo "\n----------------------------------------------------" >> backup.log
-echo "\nbacking up .bash_profile..." >> backup.log
+echo "\n----------------------------------------------------\n" >> backup.log
+cowsay "backing up .bash_profile..." >> backup.log
 cp -v ~/.bash_profile . >> backup.log
-echo "\nDone!\n" >> backup.log
+echo "\n----------------------------------------------------\n" >> backup.log
+cowsay "Done!" >> backup.log
 
 git status
 less backup.log
