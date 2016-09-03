a389ea6df764d19ec7f51c2715f0ebd37ae34cf5
Zach Gulde <zachgulde@gmail.com>
2016-09-02 22:37:20 -0500

    add script to download all my vim plugins

---
+++ get-vim-plugins.sh
@@ -0,0 +1,16 @@
+cd ~/.vim/bundle
+
+git clone https://github.com/jiangmiao/auto-pairs.git
+git clone https://github.com/kien/ctrlp.vim.git
+git clone https://github.com/mattn/emmet-vim.git
+git clone https://github.com/majutsushi/tagbar.git
+git clone https://github.com/SirVer/ultisnips.git
+git clone https://github.com/tpope/vim-commentary.git
+git clone https://github.com/junegunn/vim-easy-align.git
+git clone https://github.com/junegunn/vim-easy-align.git
+git clone https://github.com/jonathanfilip/vim-lucius.git
+git clone https://github.com/terryma/vim-multiple-cursors.git
+git clone https://github.com/jpalardy/vim-slime.git
+git clone https://github.com/tpope/vim-surround.git
+
+cd -
