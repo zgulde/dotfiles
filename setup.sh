7fc5d6beb77b474c7db0408530cf6aea362a6380
Zach Gulde <zachgulde@gmail.com>
2017-06-16 13:47:46 -0500

    add editorconfig, setup script tweaks

--- setup.sh
+++ setup.sh
@@ -6,11 +6,13 @@
 
 DOTFILES="$HOME/dotfiles"
 
-if [[ ! -d $HOME/dotfiles ]]; then
-    git clone https://github.com/zgulde/dotfiles.git $DOTFILES
+if [[ -d $DOTFILES ]]; then
+	echo "$DOTFILES directory already present! Aborting..."
+	exit 1
 fi
 
-ln -s $DOTFILES/vimrc $HOME/.vimrc
+git clone https://github.com/zgulde/dotfiles.git $DOTFILES
+
 ln -s $DOTFILES/emacs $HOME/.emacs
 
 ln -s $DOTFILES/bashrc $HOME/.bashrc
@@ -22,6 +24,10 @@ ln -s $DOTFILES/gitconfig $HOME/.gitconfig
 
 ln -s $DOTFILES/tmux.conf $HOME/.tmux.conf
 
+ln -s $DOTFILES/vimrc $HOME/.vimrc
+
+ln -s $DOTFILES/editorconfig $HOME/.editorconfig
+
 # neovim
 mkdir -p $HOME/.config/nvim
 ln -s $HOME/.vimrc $HOME/.config/nvim/init.vim
@@ -29,7 +35,7 @@ ln -s $DOTFILES/snippets/vim $HOME/.config/nvim/UltiSnips # snippets
 ln -s $DOTFILES/after $HOME/.config/nvim/after # filetype config
 # install vim-plug
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+	https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 
 mkdir -p $HOME/.emacs.d
